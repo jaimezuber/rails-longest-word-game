3f3fc275c3e95ee5bb7a6fa7dae53eb89e371516
jaime zuber <jaimezuber99@gmail.com>
2022-05-19 21:56:09 -0300

    hasta los opcionales

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,26 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    @vari = []
-    8.times { @vari.push((65 + rand(25)).chr) }
+    @letters = []
+    8.times { @letters.push((65 + rand(25)).chr) }
   end
 
   def score
-    raise
+    @word = params[:word]
+    @state = false
+    @word.split('').each do |letter|
+      @state = params[:letters].downcase.include?(letter)
+    end
+    @state = checkword(@word) if @state
+  end
+
+  private
+
+  def checkword(attempt)
+    api_url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+    user = URI.open(api_url).read
+    contenido = JSON.parse(user)
+    contenido['found']
   end
 end
